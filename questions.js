2f0067b7e90b1c9fbde2821c0a618668f01632c1
Nathan Szelag <nszelag@live.com>
2019-05-09 19:40:27 -0400

    Changed divs to sections and started a11y changes

---
+++ questions.js
@@ -0,0 +1,73 @@
+let questions = [
+    {
+        question: 'Who invented the printing press?',
+        choiceA: 'Henry Ford',
+        choiceB: 'Alexander Bell',
+        choiceC: 'Johannes Gutenberg',
+        choiceD: 'James Watt',
+        correct: 'C'
+    },     {
+        question: 'What are the three primary colors?',
+        choiceA: 'Cyan, Magenta, Yellow',
+        choiceB: 'Red, Yellow, Blue',
+        choiceC: 'Red, Green, Blue',
+        choiceD: 'Green, Orange, Purple',
+        correct: 'B'
+    } ,    {
+        question: 'Times New Roman is an example of what Typeface?',
+        choiceA: 'Serif',
+        choiceB: 'San-Serif',
+        choiceC: 'Script',
+        choiceD: 'Monospace',
+        correct: 'A'
+    },    {
+        question: 'Which of these company logos is Paul Rand famous for designing?',
+        choiceA: 'Apple',
+        choiceB: 'Microsoft',
+        choiceC: 'FedEx',
+        choiceD: 'IBM',
+        correct: 'D'
+    },    {
+        question: 'Which program should be used to design a logo?',
+        choiceA: 'Adobe Photoshop',
+        choiceB: 'Adobe Illustrator',
+        choiceC: 'Adobe Indesign',
+        choiceD: 'Microsoft Paint',
+        correct: 'B'
+    },    {
+        question: 'What colors are used for printing?',
+        choiceA: 'Red, Green, Blue, Black',
+        choiceB: 'Blue, Purple, Orange',
+        choiceC: 'Cyan, Magenta, Yellow, Black',
+        choiceD: 'Green, Violet, White, Black',
+        correct: 'C'
+    },    {
+        question: 'In which country was the Bauhaus located?',
+        choiceA: 'Germany',
+        choiceB: 'Finland',
+        choiceC: 'Switzerland',
+        choiceD: 'France',
+        correct: 'A'
+    },    {
+        question: 'What is Kerning?',
+        choiceA: 'The space between lines of type',
+        choiceB: 'The proportional spacing between characters',
+        choiceC: 'The line upon which the letter sits',
+        choiceD: 'The height of a capital letter above the baseline',
+        correct: 'B'
+    },    {
+        question: 'Which typeface designer is famous for creating Helvetica?',
+        choiceA: 'John Baskerville',
+        choiceB: 'Erik Spiekermann',
+        choiceC: 'Max Miedinger',
+        choiceD: 'Claude Garamond',
+        correct: 'C'
+    },    {
+        question: 'In what year was Adobe Photoshop released?',
+        choiceA: '1988',
+        choiceB: '1991',
+        choiceC: '1989',
+        choiceD: '1990',
+        correct: 'A'
+    }
+]
